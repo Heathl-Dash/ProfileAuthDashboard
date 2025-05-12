5b9aef07d5ea6de95ffb08b8352a78f902d55c74
Linkzandro <lizandrolgui123@gmail.com>
2025-05-12 11:04:07 -0300

    feature: model profile

--- profileCore/models.py
+++ profileCore/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Profile(AbstractUser):
+    SEX_CHOICES=((1,'F'),
+                 (2,'M'))
+    
+    BLOOD_TYPES=((1,'A+'),(2,'A-')
+                 (3,'B+'),(4,'B-')
+                 (5,'AB+'),(6,'AB-'),
+                 (7,'O+'),(8,'O-'))
+    name=models.CharField(max_length=144)
+    email=models.EmailField(max_length=255,unique=True)
+    username=None
+    sex=models.IntegerField(choices=SEX_CHOICES)
+    weigth=models.DecimalField(decimal_places=2,max_digits=5)
+    heigth=models.DecimalField(decimal_places=2,max_digits=3)
+    emergency_phone_number=models.CharField(10)
+    blood_type=models.IntegerField(choices=BLOOD_TYPES)
+    USERNAME_FIELD=email
+    REQUIRED_FIELDS=[]
+
+
+class Allergy(models.Model):
+    name=models.CharField(max_length=144)
+    allergy_type=models.IntegerField(default=1)
+    profile=models.ForeignKey(Profile,on_delete=models.CASCADE)

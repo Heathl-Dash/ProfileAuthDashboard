0281657146d1299617474333663dbc7f99d8b460
Linkzandro <lizandrolgui123@gmail.com>
2025-07-30 22:32:46 -0300

    fix: database name

--- profileCore/materialized_views/weigth_history_month_mv.py
+++ profileCore/materialized_views/weigth_history_month_mv.py
@@ -6,7 +6,7 @@ class WeigthHistoryMonthly(MaterializedView):
     sql="""
         CREATE MATERIALIZED VIEW IF NOT EXISTS mv_peso_mensal AS
         SELECT *
-        FROM seu_app_weigthhistory
+        FROM "profileCore_weigthhistory"
         WHERE DATE_TRUNC('month', created_at) = DATE_TRUNC('month', CURRENT_DATE - INTERVAL '1 month');
     """
     frequency="monthly"
